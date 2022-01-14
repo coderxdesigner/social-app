b5bfc1e8a9b02de07d6d7ce5c6680c4f923cbf24
Rachel <Rachel.Heneault@gmail.com>
2022-01-14 05:09:50 -0500

    created components for header,footer, and guest main, added year copyright

--- app/components/Header.js
+++ app/components/Header.js
@@ -13,7 +13,7 @@ function Header() {
         <form className="mb-0 pt-2 pt-md-0">
           <div className="row align-items-center">
             <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autocomplete="off" />
+              <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
             </div>
             <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
               <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
