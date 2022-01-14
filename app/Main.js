1a40486d73a7967c689099c1a7fc3fbfc631c2df
Rachel <Rachel.Heneault@gmail.com>
2022-01-14 08:33:06 -0500

    added React Router LINK SWITCH ROUTE

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 //my components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path="/" exact element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" elements={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 ReactDOM.render(<Main />, document.querySelector("#app"))
