9a36b987b0b1d020e18c9056a6059b20e1603b27
Rachel <Rachel.Heneault@gmail.com>
2022-01-16 09:10:23 -0500

    axios and connections

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,18 @@
 import React from "react"
 import Page from "./Page"
+import Axios from "axios"
 
 function HomeGuest() {
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      await Axios.post("http://localhost:8080/register", { username: "rachelTest", email: "racheltest@test.com", password: "qwertyqwertyqwertyqwertyqwertyqwerty" })
+      console.log("SUCCESS")
+    } catch (e) {
+      console.log("there was an error")
+    }
+  }
+
   return (
     <Page title="Welcome" wide={true}>
       <div className="row align-items-center">
@@ -10,7 +21,7 @@ function HomeGuest() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
