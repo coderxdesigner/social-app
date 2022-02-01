d5d1a8e149dec0ddfc24b8ad65d5822df42b6e6c
Rachel <Rachel.Heneault@gmail.com>
2022-02-01 08:26:11 -0500

    added UseImmer for the UseReducer to make it easier to manage state

--- app/Main.js
+++ app/Main.js
@@ -1,5 +1,6 @@
 import React, { useState, useReducer } from "react"
 import ReactDOM from "react-dom"
+import { useImmerReducer } from "use-immer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Axios from "axios"
 Axios.defaults.baseURL = "http://localhost:8080"
@@ -24,17 +25,20 @@ function Main() {
     loggedIn: Boolean(localStorage.getItem("complexappToken")),
     flashMessages: []
   }
-  function ourReducer(state, action) {
+  function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        return { loggedIn: true, flashMessages: state.flashMessages }
+        draft.loggedIn = true
+        return
       case "logout":
-        return { loggedIn: false, flashMessages: state.flashMessages }
+        draft.loggedIn = false
+        return
       case "flashMessages":
-        return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+        draft.flashMessages.push(action.value)
+        return
     }
   }
-  const [state, dispatch] = useReducer(ourReducer, initialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
