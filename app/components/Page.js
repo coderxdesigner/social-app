d44f352083bd95b3e1b5ea59cd9046d2efdce6ec
Rachel <Rachel.Heneault@gmail.com>
2022-02-06 07:47:10 -0500

    Added in the single post data and user details

--- app/components/Page.js
+++ app/components/Page.js
@@ -5,7 +5,7 @@ function Page(props) {
   useEffect(() => {
     document.title = `${props.title} | ComplexApp`
     window.scrollTo(0, 0)
-  }, [])
+  }, [props.title])
   return <Container wide={props.wide}>{props.children}</Container>
 }
 
--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,16 +1,45 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
 import Page from "./Page"
+import Axios from "axios"
 
 function ViewSinglePost() {
+  const { id } = useParams()
+  const [isLoading, setIsLoading] = useState(true)
+  const [post, setPost] = useState()
+
+  useEffect(() => {
+    async function getPost() {
+      try {
+        const response = await Axios.get(`/post/${id}`)
+        console.log(response.data)
+        setPost(response.data)
+        setIsLoading(false)
+      } catch (e) {
+        console.log("Ooops I did it again!")
+      }
+    }
+    getPost()
+  }, [])
+
+  if (isLoading)
+    return (
+      <Page title="Posts Loading">
+        <div>Loading...</div>
+      </Page>
+    )
+  const date = new Date(post.createdDate)
+  const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+
   return (
-    <Page title="Your Post">
+    <Page title={post.title}>
       <div className="container container--narrow py-md-5">
         <div className="d-flex justify-content-between">
-          <h2>Example Post Title</h2>
+          <h2>{post.title}</h2>
           <span className="pt-2">
-            <a href="#" className="text-primary mr-2" title="Edit">
+            <Link to="#" className="text-primary mr-2" title="Edit">
               <i className="fas fa-edit"></i>
-            </a>
+            </Link>
             <a className="delete-post-button text-danger" title="Delete">
               <i className="fas fa-trash"></i>
             </a>
@@ -18,18 +47,13 @@ function ViewSinglePost() {
         </div>
 
         <p className="text-muted small mb-4">
-          <a href="#">
-            <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
-          </a>
-          Posted by <a href="#">brad</a> on 2/10/2020
+          <Link to={`/profile/${post.author.username}`}>
+            <img className="avatar-tiny" src={post.author.avatar} />
+          </Link>
+          Posted by <Link to={`/profile/${post.author.username}`}>{post.author.username}</Link> on {dateFormatted}
         </p>
 
-        <div className="body-content">
-          <p>
-            Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit. Iure ea at esse, tempore qui possimus soluta impedit natus voluptate, sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-          </p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore qui possimus soluta impedit natus voluptate, sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.</p>
-        </div>
+        <div className="body-content">{post.body}</div>
       </div>
     </Page>
   )
