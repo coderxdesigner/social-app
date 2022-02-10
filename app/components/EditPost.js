1a39c5668e8a7ad7a7f934d7ca96df00242e0d0d
Rachel <Rachel.Heneault@gmail.com>
2022-02-10 06:42:01 -0500

    Updated the edit post feature

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { useImmerReducer } from "use-immer"
 import { useParams, Link } from "react-router-dom"
 import Page from "./Page"
 import Axios from "axios"
 import Loading from "./LoadingDotsIcon"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 function EditPost() {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
   const originalState = {
     title: {
       value: "",
@@ -29,9 +33,28 @@ function EditPost() {
         draft.body.value = action.value.body
         draft.isFetching = false
         return
+      case "titleChange":
+        draft.title.value = action.value
+        return
+      case "bodyChange":
+        draft.body.value = action.value
+        return
+      case "submitRequest":
+        draft.sendCount++
+        return
+      case "saveRequestStarted":
+        draft.isSaving = true
+        return
+      case "saveRequestDone":
+        draft.isSaving = false
+        return
     }
   }
   const [state, dispatch] = useImmerReducer(ourReducer, originalState)
+  function submitHandler(e) {
+    e.preventDefault()
+    dispatch({ type: "submitRequest" })
+  }
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source()
     async function getPost() {
@@ -49,6 +72,27 @@ function EditPost() {
     }
   }, [])
 
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: "saveRequestStarted" })
+      const ourRequest = Axios.CancelToken.source()
+      async function getPost() {
+        try {
+          const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token }, { cancelToken: ourRequest.token })
+          console.log(response.data)
+          dispatch({ type: "saveRequestDone" })
+          appDispatch({ type: "flashMessages", value: "Post was updated." })
+        } catch (e) {
+          console.log("Ooops I did it again!")
+        }
+      }
+      getPost()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.sendCount])
+
   if (state.isFetching)
     return (
       <Page title="Posts Loading">
@@ -58,22 +102,24 @@ function EditPost() {
 
   return (
     <Page title="Edit post">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          <input onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
         </div>
 
         <div className="form-group">
           <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea value={state.body.value} name="body" id="post-body" className="body-content tall-textarea form-control" type="text"></textarea>
+          <textarea onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} value={state.body.value} name="body" id="post-body" className="body-content tall-textarea form-control" type="text"></textarea>
         </div>
 
-        <button className="btn btn-primary">Save Updates</button>
+        <button className="btn btn-primary" disabled={state.isSaving}>
+          {state.isSaving ? "Saving..." : "Save Updates"}
+        </button>
       </form>
     </Page>
   )
