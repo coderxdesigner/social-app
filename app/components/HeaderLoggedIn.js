8811233b7303bc4a53d6d7f713affb33e3c4ea4a
Rachel <Rachel.Heneault@gmail.com>
2022-02-16 08:15:08 -0500

    Added css transition group for animation on overlay search

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from "react"
 import { Link } from "react-router-dom"
+import ReactTooltip from "react-tooltip"
 import DispatchContext from "../DispatchContext"
 import StateContext from "../StateContext"
 
@@ -15,19 +16,22 @@ function HeaderLoggedIn(props) {
   }
   return (
     <div className="flex-row my-3 my-md-0">
-      <Link onClick={handleSearchIcon} to="#" className="text-white mr-2 header-search-icon">
+      <Link onClick={handleSearchIcon} data-for="search" data-tip="Search" to="#" className="text-white mr-2 header-search-icon">
         <i className="fas fa-search"></i>
       </Link>
-      <span className="mr-2 header-chat-icon text-white">
+      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />{" "}
+      <span data-for="chat" data-tip="Chat" className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
-      <Link to={`/profile/${appState.user.username}`} className="mr-2">
+      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />{" "}
+      <Link data-for="profile" data-tip="My Profile" to={`/profile/${appState.user.username}`} className="mr-2">
         <img className="small-header-avatar" src={appState.user.avatar} />
       </Link>
+      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />{" "}
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </Link>
+      </Link>{" "}
       <button onClick={handleLogout} className="btn btn-sm btn-secondary">
         Sign Out
       </button>
